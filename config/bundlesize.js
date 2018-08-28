d64390e01cc30f7bf422a71bacfc310b8f978b1e
simonihmig <ihmig@kaliber5.de>
2018-08-28 23:59:58 +0200

    Initial implementation

---
+++ config/bundlesize.js
@@ -0,0 +1,10 @@
+module.exports = {
+  javascript: {
+    pattern: 'assets/*.js',
+    limit: '1KB'
+  },
+  css: {
+    pattern: 'assets/*.css',
+    limit: '1MB'
+  }
+};
--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  includedCommands() {
+    return {
+      'bundlesize:test': require('./lib/commands/test')
+    };
+  },
 };
