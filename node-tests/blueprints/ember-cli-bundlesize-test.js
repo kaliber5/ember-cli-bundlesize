4ef36c5bb590a63ca9eec250cda1e679235cf400
Steven Tsao <steventsao713@gmail.com>
2019-03-18 18:18:51 -0700

    Fix fixture tests

--- node-tests/blueprints/ember-cli-bundlesize-test.js
+++ node-tests/blueprints/ember-cli-bundlesize-test.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
-const setupTestHooks = blueprintHelpers.setupTestHooks;
-const emberNew = blueprintHelpers.emberNew;
 const emberGenerate = blueprintHelpers.emberGenerate;
+const emberNew = blueprintHelpers.emberNew;
+const fs = require('fs');
+const setupTestHooks = blueprintHelpers.setupTestHooks;
 
 const expect = require('ember-cli-blueprint-test-helpers/chai').expect;
 
@@ -15,8 +16,13 @@ describe('Acceptance: ember generate and destroy ember-cli-bundlesize', function
 
     // pass any additional command line options in the arguments array
     return emberNew()
-      .then(() => emberGenerate(args, (file) => {
-        expect(file('config/bundlesize.js')).to.equal(file(`${__dirname}/../fixtures/config/bundlesize.js`));
-    }));
+      .then(() => emberGenerate(args))
+      .then(() => {
+        const bufferFromConfig = fs.readFileSync('config/bundlesize.js');
+        const bufferFromFixture = fs.readFileSync(
+          `${__dirname}/../fixtures/config/bundlesize.js`
+        );
+        expect(bufferFromConfig.equals(bufferFromFixture)).to.be.true;
+      });
   });
 });
--- node-tests/fixtures/config/bundlesize.js
+++ node-tests/fixtures/config/bundlesize.js
@@ -1,14 +1,16 @@
 'use strict';
 
 module.exports = {
-  javascript: {
-    pattern: 'assets/*.js',
-    limit: '500KB',
-    compression: 'gzip'
-  },
-  css: {
-    pattern: 'assets/*.css',
-    limit: '50KB',
-    compression: 'gzip'
+  app: {
+    javascript: {
+      pattern: 'assets/*.js',
+      limit: '500KB',
+      compression: 'gzip'
+    },
+    css: {
+      pattern: 'assets/*.css',
+      limit: '50KB',
+      compression: 'gzip'
+    }
   }
 };
